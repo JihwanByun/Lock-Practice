786365e37db9504efb77f29fcd26c27196291942
jihwan <jihwaja9091@naver.com>
2025-01-02 18:29:18 +0900

    Feat: Redis 분산 컨트롤러

--- lock/src/main/java/com/example/controller/TicketController.java
+++ lock/src/main/java/com/example/controller/TicketController.java
@@ -1,4 +1,19 @@
 package com.example.controller;
 
+
+import com.example.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ticket/")
 public class TicketController {
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDto> purchaseTicket(){
+        boolean success = redisService.
+    }
+
 }
